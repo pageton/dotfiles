2a7476b2db84ca9d6aeded74f414d10705519a95
Rio <riomahod@gmail.com>
2025-04-30 17:19:58 +0300

    fix: add whitespace and scope options to indent-blankline configuration

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -4,5 +4,14 @@ return {
 	main = "ibl",
 	opts = {
 		indent = { char = "┊" },
+		whitespace = {
+			remove_blankline_trail = false,
+		},
+		scope = {
+			enabled = true,
+			show_start = true,
+			show_end = true,
+			highlight = { "Function", "Label" },
+		},
 	},
 }
