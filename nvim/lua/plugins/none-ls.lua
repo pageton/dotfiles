7cd91b36bc9f06d8ec0ef984e2ddcaa0a6e9c263
Sadiq <riomahod@gmail.com>
2025-05-04 02:11:53 +0300

    chore: remove Biome code action integration

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,32 +1,31 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.code_actions.biome,
-				null_ls.builtins.formatting.biome,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.golines,
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.biome,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
 
-		vim.keymap.set("n", "<leader>gf", function()
-			vim.lsp.buf.format({ async = true })
-		end, { desc = "Format with LSP" })
-	end,
+    vim.keymap.set("n", "<leader>gf", function()
+      vim.lsp.buf.format({ async = true })
+    end, { desc = "Format with LSP" })
+  end,
 }
